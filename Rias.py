594ec36e2e2357a6069da0a637f446d0bb89e613
auloehcz <alecyihua@gmail.com>
2019-07-21 00:41:08 -0500

    Heat and wave equations done

--- Rias.py
+++ Rias.py
@@ -1,32 +1,50 @@
 import numpy as np
-np.set_printoptions(threshold=np.inf)
 import graph_tool.all as gt
-import scipy as sp
+import sparse as sp
 import random
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import tkinter as tk
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, GLib
 
 
-MAX_SIZE_HISTORY = 100
-
+###############################################################################
+# /? [- /\ |_ | ~|~ `/   | _\~ |\| ~|~   /\   _\~ | |\/| |_| |_ /\ ~|~ | () |\|
+###############################################################################
+# #############################################################################
+# #############################################################################
+###############################################################################
 
 class Rias:
 
 
     # Constructors ------------------------------------------------------------
 
-    def __init__(self, graph, dt=1):
-        self.g = graph
-        self.history = []
-        self.history.append(self.g.vp.value.get_array().copy())
+    def __init__(self, graph, update_rules, dt=1, ds=1):
+        self.G = graph
+        self.num_v = self.G.num_vertices()
+        self.out_degrees = self.G.get_out_degrees(np.arange(self.num_v))
+        self.update_rules = update_rules
 
         self.timestep = 0
         self.t = 0
-        self.dt = dt
+        self.delta_time = dt # only use numbers 1 / k where k is a natural number
+        self.time_dilation = 1 // self.delta_time
+        self.delta_space = ds # only use numbers 1 / k where k is a natural number
+        self.space_dilation = 1 // self.delta_space
 
-        self.adjacency_matrix = gt.adjacency(self.g, g.ep.weight)
-        self.weight_dep_matrices = self.split_matrix_by_weight(self.adjacency_matrix)
+        # dictionary of 1 dimensional state vectors
+        self.X = {key: value for key, value in self.G.vp.items()}
+        # dictionary of 1 + 1 dimensional History matrices for each state
+        self.H = {key: np.array([value.get_array()]) for key, value in self.X.items()}
+        # 2 dimensional Adjacency matrix
+        self.A = gt.adjacency(self.G, self.G.ep["space"]).toarray()
+        self.max_size_history= int(np.amax(self.A) * self.time_dilation)
+        # 2 + 1 dimensional Update matrix
+        self.U = self.create_update_matrix()
+
+        self.win = self.create_window()
+
+
 
     @classmethod
     def init_random(cls, num_attributes, min_vertices, max_vertices, min_attribute, max_attribute, min_edges, max_edges):
@@ -51,7 +69,7 @@ class Rias:
         g = gt.Lattice([length, width], periodic=periodic)
         num_vertices = g.num_vertices()
         vals = [l for lst in lattice for l in lst]
-        g.vertex_properties["value"] = g.new_vertex_property("double", vals=vals)
+        g.vp["state"] = g.new_vp("double", vals=vals)
         return cls(g)
 
     @classmethod
@@ -61,7 +79,7 @@ class Rias:
         g = gt.Lattice([length, width], periodic=periodic)
         num_vertices = g.num_vertices()
         vals = [l for lst in lattice for l in lst]
-        g.vertex_properties["value"] = g.new_vertex_property("double", vals=vals)
+        g.vp["state"] = g.new_vp("double", vals=vals)
         for i in range(length - (0 if periodic else 1)):
             for j in range(width - (0 if periodic else 1)):
                 vertex_current = i * length + j
@@ -71,78 +89,147 @@ class Rias:
         return cls(g)
 
     @classmethod
-    def init_lattice_1d(cls, lattice, periodic=True):
-        length = len(lattice)
-        g = gt.Lattice([length], periodic=periodic)
-        num_vertices = g.num_vertices()
-        g.vertex_properties["value"] = g.new_vertex_property("double", vals=lattice)
-        return cls(g)
+    def init_lattice_1d(cls, N, vp_dict, update_rules, dt=1, ds=1, periodic=True):
+        g = gt.lattice([N], periodic=periodic)
+        for prop, vals in vp_dict.items():
+            g.vp[prop] = g.new_vp("double", vals=vals)
+        g.ep["space"] = g.new_ep("double", val=1)
+        if not periodic:
+            g.self_loops = True
+            g.add_edge_list([(0, 0), (N-1, N-1)])
+        return cls(g, update_rules, dt, ds)
 
 
 
     # Core functions -------------------------------------------------------
 
-    def update_world(self):
-        effective_values = self.kernel.dot(self.g.vs['value'])
-        for i, e_f in enumerate(effective_values):
-            v = self.g.vs[i]['value']
-            self.g.vs[i]['value'] = self.combine_func(v, e_f)
+    def update_reality(self):
+        for rule in self.update_rules:
+            first_property = rule[0] # target to update
+            second_property = rule[1] # source to take the second derivative of
+            n_antiderivatives = rule[2] # how many antiderivatives first_property has
+            constant = rule[3] # multiplicative factor for first_property's first derivative
 
-        self.t += self.dt
+            # find delta to update first_property using second_property's
+            # second derivative
+            current_state = self.X[second_property].get_array()
+            gifts_from_neighbors = np.zeros(self.num_v)
+            for i, state in reversed(list(enumerate(self.H[second_property]))):
+                gifts_from_neighbors += self.U[i].dot(state)
+            delta = constant * (current_state - gifts_from_neighbors)
+
+            # Adjust velocities using data from positions
+            self.X[first_property].a += delta * self.delta_space
+
+            # FIX!!-
+            # update second property's antiderivatives using updated first_property
+            for i in range(n_antiderivatives):
+                self.X[second_property].a += self.X[first_property].a
+            for i in range(n_antiderivatives+1):
+                self.H[first_property] = np.vstack((self.H[first_property],
+                                                    self.X[first_property].a))
+            self.H[second_property] = np.vstack((self.H[second_property],
+            self.X[second_property].a))
+
+            if len(self.H[first_property]) > self.max_size_history:
+                self.H[first_property] = np.delete(self.H[first_property], (0), axis=0)
+            if len(self.H[second_property]) > self.max_size_history:
+                self.H[second_property]= np.delete(self.H[second_property], (0), axis=0)
+
+
+
+        self.t += self.delta_time
         self.timestep += 1
+        print(self.timestep)
 
-    def split_matrix_by_weight(self, adjacency_matrix):
-        matrices = {}
-        f = lambda x, i: 1 if x == i else 0
-        for i, row in enumerate(adjacency_matrix):
-            for j, item in enumerate(row):
-                if item != 0:
-                    matrices[item] = matrices.get(item, sp.csr_matrix(adjacency_matrix.shape)
-                    matrices[item][i][j] = 1
-                else:
-                    continue
-        return matrices
-
-
+    def create_update_matrix(self):
+        coordinates = []
+        data = []
+        for i, row in enumerate(self.A):
+            for j, edge_length in enumerate(row):
+                coordinates.append((edge_length*self.time_dilation-1, i, j))
+                data.append(1 / self.out_degrees[j] / self.time_dilation) # / self.dilation
+        coordinates = list(zip(*coordinates))
+        U = sp.COO(coordinates, data, shape=(self.max_size_history, self.num_v, self.num_v))
+        return U
 
 
+    # Visualization -----------------------------------------------------------
 
+    def create_window(self):
+        pos = gt.planar_layout(self.G)
+        win = gt.GraphWindow(self.G, pos, geometry=(100, 100),
+                             vertex_fill_color=self.G.vp["position"])
+        return win
 
+    def update_window(self):
+        self.update_reality()
+        self.win.graph.regenerate_surface()
+        self.win.graph.queue_draw()
+        return True
 
-    # Utility functions -------------------------------------------------------
+    def animate_graph(self):
+        # Bind the function above as an 'idle' callback.
+        cid = GLib.idle_add(self.update_window)
 
+        # We will give the user the ability to stop the program by closing the window.
+        self.win.connect("delete_event", Gtk.main_quit)
 
+        # Actually show the window, and start the main loop.
+        self.win.show_all()
+        Gtk.main()
 
-    # Presets -----------------------------------------------------------------
+    def animate_plt(self):
+        fig = plt.figure()
+        ax = plt.axes(xlim=(0, 1000), ylim=(-1000, 1000))
+        line, = ax.plot([], [], lw=1)
 
-    @staticmethod
-    def kernel_func_heat(distance):
-        if distance <= 1:
-            return 1
-        else:
-            return 0
+        pause = False
 
+        def onClick(event):
+            global pause
+            pause = True
+        fig.canvas.mpl_connect('button_press_event', onClick)
 
+        # initialization function: plot the background of each frame
+        def init():
+            line.set_data([], [])
+            return line,
 
+        # animation function.  This is called sequentially
+        def animate(i):
+            if not pause:
+                self.update_reality()
+                ax.annotate('timestep = ' + str(self.timestep), (100, 100))
+                x = np.linspace(0, 1000, 1000)
+                y = np.fft.fft(self.X['position'].get_array())
+                line.set_data(x, y)
+            return line,
 
-def zipf(N, k, s):
-    return 1 / ((k ** s) * sum([1 / n ** s for n in range(1, N + 1)]))
-
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                    frames=200, interval=20, blit=True)
+        plt.show()
 
 
 def main():
-    kernel_func = Rias.kernel_func_life
-    initial_lattice_2d_8n = np.floor(np.random.rand(100, 100) * 10) % 2
-    world = Rias.init_lattice_2d_8n(initial_lattice_2d_8n, kernel_func)
-    color_dict = {0: (0,0,0), 1: (255,255,255)}
-    layout = world.g.layout('grid')
-    while True:
-        print(world.timestep)
-        plot(world.g, layout = layout, vertex_color = [color_dict[v] for v in world.g.vs['value']])
-        world.update_world()
-#    initial_lattice_1d = np.random.rand(1000)
-#    world = Rias.init_lattice_1d(initial_lattice_1d, periodic=False)
-#    layout = world.g.layout('circle')
+    initial_states = {'position': (np.random.rand(1000) - 0.5) * 10,
+                      'velocity': np.concatenate(((np.random.rand(500) - 0.5) * 5, np.zeros((500))))}
+    np.concatenate(([0], initial_states['position'], [0]))
+    np.concatenate(([0], initial_states['velocity'], [0]))
+    #initial_states = {'position': np.fromfunction(lambda x: 10*np.sin(np.pi*x/500) + 1000, (1000,)),
+    #                    'velocity':np.fromfunction(lambda x: 10*np.cos(np.pi*x/500) + 25, (1000,))}
+
+    reality = Rias.init_lattice_1d(1000, initial_states, [('velocity', 'position', 1, -1)],
+                                   dt = 1/50, ds = 1/50, periodic=False)
+
+
+    #initial_states = {'position': (np.random.rand(1000) - 0.5) * 500}
+    #reality = Rias.init_lattice_1d(1000, initial_states, [('position', 'position', 0, -1)],
+    #                               dt = 1/50, ds = 1/50, periodic=True)
+
+    reality.animate_plt()
+
 
 
 
