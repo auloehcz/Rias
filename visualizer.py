7128888ce81ae7b058faad2b6a3ffeb2baca76b0
auloehcz <alecyihua@gmail.com>
2019-09-01 20:14:29 -0700

    working on lattice

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,10 @@
 import dynamical_graph as dg
+import presets
+import graph_tool.all as gt
 import numpy as np
-np.set_printoptions(threshold=np.inf)
+import random
+import collections
+#np.set_printoptions(threshold=np.inf)
 
 from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, GLib
 import matplotlib
@@ -37,51 +41,57 @@ class MainWindow(Gtk.Window):
         #self.notebook.append_page(self.page2, label2)
 
 
-
 class Box2D(Gtk.VBox):
     def __init__(self):
         Gtk.VBox.__init__(self)
-        self.dg = None
-
         # Controls ------------------------------------------------------------
         self.controls = {}
         self.grid_controls = Gtk.Grid()
         self.pack_start(self.grid_controls, False, False, 0)
-        # Pause/unpause
-        self.paused = True
-        self.controls['button_pause'] = Gtk.Button.new_with_label("start")
-        self.controls['button_pause'].connect("clicked", self.pause)
-        self.grid_controls.attach(self.controls['button_pause'], 1, 1, 1, 1)
-        # Alpha adjustment
-        alpha = Gtk.Adjustment(value=0.5, lower=0, upper=1, step_increment=.01)
-        self.controls['button_alpha'] = Gtk.SpinButton()
-        self.controls['button_alpha'].configure(alpha, .1, 2)
-        self.grid_controls.attach(self.controls['button_alpha'], 1, 2, 2, 2)
 
         # Properties setter
-        #label_properties = Gtk.Label()
-        #label_properties.set_label('properties')
-        #self.grid_controls.attach(label_properties, 10, 1, 1, 1)
-
         self.controls['entry_num_props'] = Gtk.Entry()
         self.controls['entry_num_props'].set_text("2")
-        self.grid_controls.attach(self.controls['entry_num_props'], 5, 1, 1, 1)
+        self.grid_controls.attach(self.controls['entry_num_props'], 1, 1, 1, 1)
 
-        self.controls['button_set_props'] = Gtk.Button.new_with_label("set")
-        self.controls['button_set_props'].connect("clicked", self.set_properties)
-        self.grid_controls.attach(self.controls['button_set_props'], 6, 1, 1, 1)
+        self.controls['button_set_num_props'] = Gtk.Button.new_with_label("Set Number of Properties")
+        self.controls['button_set_num_props'].connect("clicked", self.set_num_props)
+        self.grid_controls.attach(self.controls['button_set_num_props'], 2, 1, 1, 1)
+
+        # Graph Presets
+        self.controls['combobox_graph_presets'] = Gtk.ComboBoxText.new_with_entry()
+        self.controls['combobox_graph_presets'].append('lattice', 'lattice')
+        self.controls['combobox_graph_presets'].connect("changed", self.on_preset_changed)
+        self.controls['combobox_graph_presets'].set_entry_text_column(1)
+        self.grid_controls.attach(self.controls['combobox_graph_presets'], 1, 3, 1, 1)
 
 
-        # Reset
+        # Kernel setter
+        self.controls['button_set_kernels'] = Gtk.Button.new_with_label("Set Kernels")
+        self.controls['button_set_kernels'].connect("clicked", self.create_window_kernels)
+        self.grid_controls.attach(self.controls['button_set_kernels'], 1, 5, 1, 1)
 
-        # Presets
-        liststore_presets = Gtk.ListStore(int, str)
-        liststore_presets.append([1, '1d Wave'])
-        liststore_presets.append([2, '1d Heat'])
-        self.controls['combobox_presets'] = Gtk.ComboBox.new_with_model_and_entry(liststore_presets)
-        self.controls['combobox_presets'].connect("changed", self.on_name_combo_changed)
-        self.controls['combobox_presets'].set_entry_text_column(1)
-        self.grid_controls.attach(self.controls['combobox_presets'], 2, 1, 2, 1)
+
+
+
+        # Initialize graph
+        self.controls["button_initialize"] = Gtk.Button.new_with_label("Initialize")
+        self.controls['button_initialize'].connect("clicked", self.initialize_graph)
+        self.grid_controls.attach(self.controls['button_initialize'], 7, 1, 1, 1)
+
+        # Pause/unpause
+        self.paused = True
+        self.controls['button_pause'] = Gtk.Button.new_with_label("Start")
+        self.controls['button_pause'].connect("clicked", self.pause)
+        self.grid_controls.attach(self.controls['button_pause'], 1, 1, 1, 1)
+
+        # Alpha adjustment
+        #alpha = Gtk.Adjustment(value=0.5, lower=0, upper=1, step_increment=.01)
+        #self.controls['button_alpha'] = Gtk.SpinButton()
+        #self.controls['button_alpha'].configure(alpha, .1, 2)
+        self.show_all()
+        #self.grid_controls.attach(self.controls['button_alpha'], 1, 2, 2, 2)
+
 
         # Plot ----------------------------------------------------------------
         self.plot_box = Gtk.VBox(spacing=0)
@@ -106,31 +116,158 @@ class Box2D(Gtk.VBox):
         #m.set_array(self.dg.X['position'].a)
         #cbar = plt.colorbar(m)
 
-        self.anim = animation.FuncAnimation(self.fig, self.animate, init_func=self.init_line,
-                                    frames=2000, interval=20, blit=False)
+        self.anim = animation.FuncAnimation(self.fig, self.animate,
+                                            init_func=self.init_line,
+                                            frames=2000, interval=20, blit=False)
 
 
-    def set_properties(self, button):
-        num_props = int(self.controls['entry_num_props'].get_text())
-        grid_kernels = Gtk.Grid()
-        for i in range(num_props):
-            entry_prop = 'self.entry_prop' + str(i)
+
+
+    def set_num_props(self, button):
+        self.num_props = int(self.controls['entry_num_props'].get_text())
+        box_props = Gtk.HBox()
+        for i in range(self.num_props):
+            entry_prop = 'entry_prop' + str(i)
             self.controls[entry_prop] = Gtk.Entry()
-            grid_kernels.attach(self.controls[entry_prop], 1, i+1, 1, 1)
-            for j in range(num_props):
-                entry_kernel = "self.entry_kernel" + str(i) + str(j)
-                self.controls[entry_kernel] = Gtk.Entry()
-                grid_kernels.attach(self.controls[entry_kernel], j+2, i+1, 1, 1)
-        self.grid_controls.attach(grid_kernels, 7, 1, num_props, num_props+1)
+            box_props.pack_end(self.controls[entry_prop], True, True, 0)
+        self.controls['button_set_prop_names'] = Gtk.Button.new_with_label("Set Property Names")
+        self.controls['button_set_prop_names'].connect("clicked", self.set_prop_names)
+        self.grid_controls.remove_row(2)
+        self.grid_controls.insert_row(2)
+        self.grid_controls.attach(box_props, 1, 2, 1, 1)
+        self.grid_controls.attach(self.controls['button_set_prop_names'], 2, 2, 1, 1)
         self.show_all()
 
+    def set_prop_names(self, button):
+        self.prop_names = []
+        for i in range(self.num_props):
+            s = 'entry_prop' + str(i)
+            self.prop_names.append(self.controls[s].get_text())
 
-    def on_name_combo_changed(self, preset):
+
+
+    def initialize_graph(self, button):
+        graph_preset = self.controls['combobox_graph_presets'].get_entry_text_column()
+        g = graph_preset
+        self.dg = dg.DynamicalGraph(self.g, self.time_kernels, self.space_kernels)
+
+
+    def create_window_kernels(self, button):
+        preset_kernels = [f for f in dir(presets.KernelFuncs) if "__" not in f]
+        label_space = Gtk.Label(label='Space Kernels')
+        grid_space_kernels = Gtk.Grid()
+        hbox_space_labels = Gtk.HBox()
+        vbox_space_labels = Gtk.VBox()
+        label_time = Gtk.Label(label='Time Kernels')
+        grid_time_kernels = Gtk.Grid()
+        hbox_time_labels = Gtk.HBox()
+        vbox_time_labels = Gtk.VBox()
+        for i in range(self.num_props):
+            label = self.prop_names[i]
+            hbox_space_labels.pack_end(Gtk.Label(label=label), True, True, 0)
+            vbox_space_labels.pack_end(Gtk.Label(label=label), True, True, 0)
+            hbox_time_labels.pack_end(Gtk.Label(label=label), True, True, 0)
+            vbox_time_labels.pack_end(Gtk.Label(label=label), True, True, 0)
+            for j in range(self.num_props):
+                combobox_space = "combobox_space_kernel" + str(i) + str(j)
+                combobox_time = "combobox_time_kernel" + str(i) + str(j)
+                self.controls[combobox_space] = Gtk.ComboBoxText()
+                self.controls[combobox_time] = Gtk.ComboBoxText()
+                for kernel in preset_kernels:
+                    self.controls[combobox_space].append(kernel, kernel)
+                    self.controls[combobox_time].append(kernel, kernel)
+                grid_space_kernels.attach(self.controls[combobox_space], j+2, i+1, 1, 1)
+                grid_time_kernels.attach(self.controls[combobox_time], j+2, i+1, 1, 1)
+        button_set_kernels = Gtk.Button.new_with_label("Set Kernels")
+        button_set_kernels.connect("clicked", self.set_kernels)
+
+        grid_kernels = Gtk.Grid()
+        grid_kernels.attach(label_space, 2, 1, 1, 1)
+        grid_kernels.attach(hbox_space_labels, 2, 2, 1, 1)
+        grid_kernels.attach(vbox_space_labels, 1, 3, 1, 1)
+        grid_kernels.attach(grid_space_kernels, 2, 3, 1, 1)
+        grid_kernels.attach(label_time, 2, 4, 1, 1)
+        grid_kernels.attach(hbox_time_labels, 2, 5, 1, 1)
+        grid_kernels.attach(vbox_time_labels, 1, 6, 1, 1)
+        grid_kernels.attach(grid_time_kernels, 2, 6, 1, 1)
+        grid_kernels.attach(button_set_kernels, 2, 7, 1, 1)
+
+        self.window_kernels = Gtk.Window()
+        self.window_kernels.set_title("Kernel Controls")
+        self.window_kernels.connect("destroy", lambda *w: Gtk.main_quit)
+        self.window_kernels.set_modal(True)
+        self.window_kernels.add(grid_kernels)
+        self.window_kernels.show_all()
+
+
+
+    def set_kernels(self, button):
+        self.space_kernels = {}
+        self.time_kernels = {}
+        for i in range(self.num_props):
+            self.space_kernels[self.prop_names[i]] = {}
+            self.time_kernels[self.prop_names[i]] = {}
+            for j in range(self.num_props):
+                entry_name = "entry_space_kernel" + str(i) + str(j)
+                self.space_kernels[self.prop_names[i]][self.prop_names[j]] = \
+                    self.controls[entry_name].get_text()
+                entry_name = "entry_time_kernel" + str(i) + str(j)
+                self.space_kernels[self.prop_names[i]][self.prop_names[j]] = \
+                    self.controls[entry_name].get_text()
+
+        self.window_kernels.gtk_window_close()
+
+    def on_preset_changed(self, preset):
+        preset = self.controls['combobox_graph_presets'].get_active_text()
+        if preset == 'lattice':
+            self.create_lattice_controls()
+        else:
+            print('Not implemented yet!')
+
         return
+
+    def create_lattice_controls(self):
+        self.grid_lattice_controls = Gtk.Grid()
+        for i in range(self.num_props):
+            s0 = 'entry_initial_state' + str(i)
+            s1 = 'entry_boundary_condition' + str(i)
+            self.controls[s0] = Gtk.Entry()
+            self.controls[s1] = Gtk.Entry()
+            self.grid_lattice_controls.attach(self.controls[s0], i+1, 1, 1, 1)
+            self.grid_lattice_controls.attach(self.controls[s1], i+1, 2, 1, 1)
+        self.controls['entry_shape'] = Gtk.Entry()
+        self.controls['entry_max_radius'] = Gtk.Entry()
+        self.controls['checkbutton_periodic'] = Gtk.CheckButton.new_with_label("Periodic")
+        self.controls["button_set_graph"] = Gtk.Button.new_with_label("Set Graph")
+        self.controls['button_set_graph'].connect("clicked", self.set_graph_lattice)
+        self.grid_lattice_controls.attach(self.controls['entry_shape'], 3, 1, 1, 1)
+        self.grid_lattice_controls.attach(self.controls['entry_max_radius'], 3, 1, 1, 1)
+        self.grid_lattice_controls.attach(self.controls['checkbutton_periodic'], 4, 1, 1, 1)
+        self.grid_lattice_controls.attach(self.controls['button_set_graph'], 10, 10, 1, 1)
+
+        self.grid_controls.attach(self.grid_lattice_controls, 1, 4, 1, 1)
+        self.show_all()
+
+    def set_graph_lattice(self, button):
+        periodic = self.controls['checkbutton_periodic'].get_active()
+        shape = eval(self.controls["entry_shape"].get_text())
+        max_radius = int(self.controls['entry_max_radius'].get_text())
+        self.g = gt.lattice(shape, periodic=periodic)
+        for i in range(self.num_props):
+            f = self.controls['entry_initial_state' + str(i)]
+            self.g.vp[self.prop_names[i]] = list(self.flatten(np.from_function(f, shape)))
+            #for v in
 
     def create_plot(self):
         return
 
+
+    def flatten(self, l):
+        for el in l:
+            if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
+                yield from self.flatten(el)
+            else:
+                yield el
 
     def pause(self, button):
         self.paused = not self.paused
@@ -156,6 +293,7 @@ class Box2D(Gtk.VBox):
         y2 = self.dg.X['velocity'].a
         self.line2.set_data(x, y2)
         return self.line1, self.line2, self.timestamp
+
 
 
 class Box3D(Gtk.VBox):
